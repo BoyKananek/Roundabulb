e2380c737842c91f22eea1980d1db514006ec81d
BoyKananek <kananek.ati@hotmail.com>
2016-12-30 00:31:32 +0700

    edit call back url

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1218086994893686', // your App ID
         'clientSecret'  : 'ec1aef6abd6ba4e3e5cec21b03e56f8b', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+        'callbackURL'   : 'https://roundabulb.herokuapp.com/auth/facebook/callback'
     }
 }
